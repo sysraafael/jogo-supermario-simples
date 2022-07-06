e2f28c7cab6038222006aa2a5afcda2adadb472a
sysraafael <101739492+sysraafael@users.noreply.github.com>
2022-07-06 18:39:10 -0300

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,4 @@
-// Função para mutar a música tema.
-function mute() {
-    music.muted = true;
-    BTN_MUTE.style.display = 'block';
-    BTN_UP.style.display = 'none';
-  }
-  // Função para remover o mute da música.
-  function up() {
-    music.muted = false;
-    BTN_MUTE.style.display = 'none';
-    BTN_UP.style.display = 'block';
-  }
-  
-  /* Música Tema */
+/* Música Tema */
   let music = document.getElementById("musicTheme");
   
   // Tocar a música ao interagir com o corpo do site.
